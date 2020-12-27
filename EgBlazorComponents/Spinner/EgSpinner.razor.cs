44c69bbc2dd6db7c2913b7e299e3fbbb70b5c4c7
eugenekim <eugene.kimley@gmail.com>
2020-12-27 23:22:27 +1100

    Added the spinner component and demo.

---
+++ EgBlazorComponents/Spinner/EgSpinner.razor.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Threading.Tasks;
+
+namespace EgBlazorComponents.Spinner
+{
+	public partial class EgSpinner : EgComponentBase
+	{
+		private bool showSpinner;
+		private bool disposed;
+
+		/// <summary>
+		/// Show the spinner.
+		/// </summary>
+		/// <param name="action">The long running process</param>
+		/// <returns>Async operation.</returns>
+		public async Task ShowAsync(Func<Task> action)
+		{
+			Show(true);
+
+			try
+			{
+				await action();
+			}
+			finally
+			{
+				Show(false);
+			}
+		}
+
+		private void Show(bool show)
+		{
+			if (disposed)
+				return;
+
+			showSpinner = show;
+			StateHasChanged();
+		}
+
+		public override void Dispose() => disposed = true;
+	}
+}
