ca54fa8fd822a4fbdfd130a7168c3079ae818f7b
Eugene Kim <eugene.kimley@gmail.com>
2022-07-27 23:24:18 +1000

    Added the EgModal component.

---
+++ EgBlazorComponents/Args/ModalArgs.cs
@@ -0,0 +1,34 @@
+﻿namespace EgBlazorComponents.Args
+{
+	public enum ModalButtons
+	{
+		Ok,
+		Cancel,
+		OkCancel,
+		Yes,
+		YesNo,
+		None
+	}
+
+	public enum ModalResult
+	{
+		Ok,
+		Yes,
+		No,
+		Cancel
+	}
+
+	public class ModalArgs<T> where T : class
+	{
+		public ModalButtons ModalButtons { get; }
+		public T Data { get; }
+
+		public ModalArgs(T data) : this(ModalButtons.None, data) { }
+
+		public ModalArgs(ModalButtons modalButtons, T data)
+		{
+			ModalButtons = modalButtons;
+			Data = data;
+		}
+	}
+}
